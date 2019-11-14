ed78f9014a6533a9a6832bd60cf57f2855d839a6
Jamaal Ahmed <jamaal729@yahoo.com>
2019-11-14 06:16:22 +0000

    Fixed sql injection problem.

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,27 +1,34 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx"
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+
+const queryString = `
 SELECT teachers.name AS teacher, cohorts.name as cohort
 FROM teachers
   JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
   JOIN students ON assistance_requests.student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
   JOIN assignments ON assistance_requests.assignment_id = assignments.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 GROUP BY teachers.name, cohorts.name
-ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher} `);
+ORDER BY teachers.name;`;
+
+pool
+  .query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher} `);
+    });
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch(err => console.error("query error", err.stack));
 
 // USAGE: node teachers.js JUL02
